7f390e3de3cc5a91c1ebe6f3589db0ac48dc16f4
Mario Garcia <mariogarciacoa@gmail.com>
2022-12-17 06:18:30 -0400

    Second commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const logger = require("./loggerMidleware")
+
+app.use(cors())
 
 app.use(express.json())
 
@@ -28,7 +31,7 @@ let notes = [
 ]
 
 app.get("/", (req, res) => {
-	res.send("</h1>API DE NOTAS</h1>")
+	res.send("<h1>API DE NOTAS</h1>")
 })
 
 app.get("/api/notes", (req, res) => {
@@ -46,20 +49,22 @@ app.get("/api/notes/:id", (req, res) => {
 	}
 })
 
+const generateId = () => {
+	const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+	return maxId + 1
+}
+
 app.post("/api/notes/", (req, res) => {
 	const note = req.body
 
 	if (!note || !note.content) {
 		return res.status(400).json({
-			error: "note.content is required",
+			error: "content missing",
 		})
 	}
 
-	const ids = notes.map((note) => note.id)
-	const maxId = Math.max(...ids)
-
 	const newNote = {
-		id: maxId + 1,
+		id: generateId(),
 		content: note.content,
 		date: new Date().toISOString(),
 		important: typeof note.important !== "undefined" ? note.important : false,
@@ -68,6 +73,18 @@ app.post("/api/notes/", (req, res) => {
 	notes = [...notes, newNote]
 
 	res.json(newNote)
+})
+
+app.put("/api/notes/:id", (req, res) => {
+	const id = Number(req.params.id)
+	const note = notes.find((note) => note.id === id)
+
+	if (note) {
+		notes = notes.map((note) => (note.id !== id ? note : req.body))
+		res.json(req.body)
+	} else {
+		res.status(404).end("<h1>Not Found Note</h1>")
+	}
 })
 
 app.delete("/api/notes/:id", (req, res) => {
