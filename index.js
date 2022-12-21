b840a595e8d6be46d47b3f3bf6121ae59178c029
Mario Garcia <mariogarciacoa@gmail.com>
2022-12-21 15:09:20 -0400

    heroku config

--- index.js
+++ index.js
@@ -97,7 +97,7 @@ app.delete("/api/notes/:id", (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
